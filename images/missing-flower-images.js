e39df4f01a424b1ad00a806c02e02243bb077f49
yihyeone <35987100+yihyeone@users.noreply.github.com>
2026-09-14 02:15:33 +0900

    별의 마녀 보조 이미지맵 최신화

--- images/missing-flower-images.js
+++ images/missing-flower-images.js
@@ -1,1 +1,1 @@
-window.MISSING_FLOWER_IMAGES={"별의 마녀":"./images/flower-assets/8d457ebcf014252a2bee.webp","별을 쫓는 달토끼":"./images/flower-assets/96a320ff3883ab12c5e1.webp"};
+window.MISSING_FLOWER_IMAGES={"별의 마녀":"./images/flower-assets/card-star-witch-20260914.webp","별을 쫓는 달토끼":"./images/flower-assets/96a320ff3883ab12c5e1.webp"};
